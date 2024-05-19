08ed6d92dbd09fe4a06d55d70dfd97d56cf97ed3
Kazeirokie <aminulaiman@gmail.com>
2024-05-19 22:05:40 +0800

    Selection Sort FIXED

--- SelectionSort.java
+++ SelectionSort.java
@@ -32,45 +32,47 @@ public class SelectionSort implements Sort {
         sampleData2.add(2);
         sampleData2.add(4);
 
-        System.out.println(sampleData1);
-        System.out.println(sampleData2);
-
-        // ada problem kat sampleData2
-        samples.add(sampleData2);
+  
         samples.add(sampleData1);
-
-        
+        samples.add(sampleData2);
+             
 
         for (int setIndex = 0; setIndex < samples.size(); setIndex++) {
 
             System.out.println("## SAMPLE " + (setIndex + 1) + " ##");
+            System.out.println(samples.get(setIndex));
 
             int y = 0; // Bigger Item
             int swapFlag = 0; // false
             int currentItem = samples.get(setIndex).size() - 1;
             int yIndex = 0; // Bigger Item Index
-            int xIndex = samples.get(setIndex).get(currentItem); // currentItem Index
+            int x = samples.get(setIndex).get(currentItem); // currentItem
 
             long startTime = System.nanoTime();
 
             for (int i = currentItem; i >= 0; i--) {
-                xIndex = samples.get(setIndex).get(i); // set currentItem
+                x = samples.get(setIndex).get(i); // set currentItem
 
                 for (int j = 0; j < i; j++) { // move down one element each iteration
-                    if (samples.get(setIndex).get(j) > xIndex) {
-                        y = samples.get(setIndex).get(j);
-                        yIndex = j;
+                    if (samples.get(setIndex).get(j) > x) {
 
+                        // make sure biggest value taken
+                        if (samples.get(setIndex).get(j) > y) {
+                            y = samples.get(setIndex).get(j);
+                            yIndex = j;
+                        }
+                       
                         swapFlag = 1; // true
                     }
 
                     if (j == (i - 1) && swapFlag == 1) { // at last element
-                        int temp = xIndex; // temp = 8
-                        xIndex = y; // x = 9
+                        int temp = x; // temp = 8
+                        x = y; // x = 9
                         samples.get(setIndex).set(yIndex, temp); // smaller value take bigger value index
-                        samples.get(setIndex).set(i, xIndex); // smaller value take smaller value index
+                        samples.get(setIndex).set(i, x); // smaller value take smaller value index
 
                         swapFlag = 0; // reset
+                        y = 0; //reset 
                         System.out.println(samples.get(setIndex));
 
                     }
@@ -90,28 +92,33 @@ public class SelectionSort implements Sort {
             int swapFlag = 0; // false
             int currentItem = data.getSet(setIndex).getData().size() - 1;
             int yIndex = 0; // Bigger Item Index
-            int xIndex = data.getSet(setIndex).getData().get(currentItem); // currentItem Index
+            int x = data.getSet(setIndex).getData().get(currentItem); // currentItem 
 
             long startTime = System.nanoTime();
 
             for (int i = currentItem; i >= 0; i--) {
-                xIndex = data.getSet(setIndex).getData().get(i); // set currentItem
+                x = data.getSet(setIndex).getData().get(i); // set currentItem
 
                 for (int j = 0; j < i; j++) { // move down one element each iteration
-                    if (data.getSet(setIndex).getData().get(j) > xIndex) {
-                        y = data.getSet(setIndex).getData().get(j);
-                        yIndex = j;
+                    if (data.getSet(setIndex).getData().get(j) > x) {
 
+                        // make sure biggest value taken
+                        if (data.getSet(setIndex).getData().get(j) > y) {
+                            y = data.getSet(setIndex).getData().get(j);
+                            yIndex = j;
+                        }
+                       
                         swapFlag = 1; // true
                     }
 
                     if (j == (i - 1) && swapFlag == 1) { // at last element
-                        int temp = xIndex; // temp = 8
-                        xIndex = y; // x = 9
+                        int temp = x; // temp = 8
+                        x = y; // x = 9
                         data.getSet(setIndex).getData().set(yIndex, temp); // smaller value take bigger value index
-                        data.getSet(setIndex).getData().set(i, xIndex); // smaller value take smaller value index
+                        data.getSet(setIndex).getData().set(i, x); // smaller value take smaller value index
 
                         swapFlag = 0; // reset
+                        y = 0; //reset 
                         //System.out.println(data.getSet(setIndex).getData());
 
                     }
@@ -120,7 +127,7 @@ public class SelectionSort implements Sort {
 
             long endTime = System.nanoTime();
             long duration = (endTime - startTime); // in nanoseconds
-            System.out.println("Execution time in milliseconds: " + (duration) + "ns\n");
+            System.out.println("Execution time in nanoseconds: " + (duration) + "ns\n");
         }
     }
 }
